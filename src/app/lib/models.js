d21db96f47dd978911f7123b795e9c275b7d76b7
Emily <dha524@aucklanduni.ac.nz>
2026-03-29 19:00:39 +1300

    feat (api): allow users to create a group, and return the group details with a randomly generated invite code

--- src/app/lib/models.js
+++ src/app/lib/models.js
@@ -12,4 +12,31 @@ const taskSchema = new Schema(
 
 const Task = mongoose.models.Task || mongoose.model("Task", taskSchema);
 
+const groupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    inviteCode: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Group =
+  mongoose.models.Group || mongoose.model("Group", groupSchema);
+
 export default Task;
