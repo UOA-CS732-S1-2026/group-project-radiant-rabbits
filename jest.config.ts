cf799edac56bf054ee344438854ebdae841db3c5
nadiaaskari <161200674+nadiaaskari@users.noreply.github.com>
2026-05-11 19:37:23 +1200

    added jest workdlows

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
+  testTimeout: 60000,
   testMatch: ["**/*.test.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
